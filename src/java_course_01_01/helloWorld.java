0135753a4aa986f52ccb81b53a70ce575e108fd3
TWCSC\234781 <234781@mail.csc.com.tw>
2024-09-02 11:01:32 +0800

    新增java檔案

---
+++ src/java_course_01_01/helloWorld.java
@@ -0,0 +1,6 @@
+package java_course_01_01;
+public class helloWorld{
+	public static void print() {
+		System.out.println("helloWorld.java :　Hello World");
+	}
+}
---
+++ src/java_course_01_01/java_course_01_01.java
@@ -0,0 +1,7 @@
+package java_course_01_01;
+public class java_course_01_01{
+	public static void main(String[] args) {
+		System.out.println("java_course_01_01 :　Hello World");
+		helloWorld.print();
+	}
+}
